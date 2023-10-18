87eb4c74c24bdd9aabd08ffb8c8e5ca060a64351
JosephBlom <24.joseph.blom@ksd.org>
2023-10-18 13:58:10 -0700

    Created a Pause Menu Scene and Script

---
+++ 2D Group Project/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Canvas>().enabled = false;
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape) && Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+            GetComponent<Canvas>().enabled = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && Time.timeScale == 0)
+        {
+            Resume();
+        }
+
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        GetComponent<Canvas>().enabled = false;
+    }   
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
