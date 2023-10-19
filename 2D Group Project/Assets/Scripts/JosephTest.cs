c6f75d0e14acd19a71cb586246bcda91aca7779c
JosephBlom <24.joseph.blom@ksd.org>
2023-10-19 13:29:52 -0700

    Updated the player prefab

--- 2D Group Project/Assets/Scripts/JosephTest.cs
+++ 2D Group Project/Assets/Scripts/JosephTest.cs
@@ -1,42 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.SceneManagement;
 
-public class JosephTest : MonoBehaviour
+public class JosephTesting : MonoBehaviour
 {
-    Vector2 moveInput;
-    Rigidbody2D rb2d;
+    [SerializeField] GameObject player;
+    [SerializeField] float chaseDistance = 2.0f;
+    [SerializeField] float moveSpeed = 5.0f;
 
-    [SerializeField] int moveSpeed = 5;
-    [SerializeField] int jumpSpeed = 5;
-
-
-    void Start()
-    {
-        rb2d = GetComponent<Rigidbody2D>();
-    }
-
+    // Update is called once per frame
     void Update()
     {
-        Run();
-    }
-
-    void OnMove(InputValue value)
-    {
-        moveInput = value.Get<Vector2>();
-    }
-
-    void OnJump(InputValue value)
-    {
-        rb2d.velocity += new Vector2(0f, jumpSpeed);
-    }
-
-    void Run()
-    {
-        Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed, rb2d.velocity.y);
-        rb2d.velocity = playerVelocity;
+        Vector3 playerPosition = player.transform.position;
+        Vector3 moveDirection = playerPosition - transform.position;
+        //if the player is close
+        if (moveDirection.magnitude < chaseDistance)
+        {
+            moveDirection.Normalize();
+            GetComponent<Rigidbody2D>().velocity = moveDirection * moveSpeed;
+        }
+        //if the player is too far away
+        else
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+        }
     }
 }
