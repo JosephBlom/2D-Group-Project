7e640a40a8e711b2497cf723599f4742e8e24863
MarshmallowBoy <traysoncazier719@gmail.com>
2023-10-23 12:52:38 -0700

    Made Main Menu

---
+++ 2D Group Project/Assets/Scripts/UI_Functions.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI_Functions : MonoBehaviour
+{
+    public GameObject SettingsMenu;
+    void Start()
+    {
+        SettingsMenu.SetActive(false);
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void Play()
+    {
+        SceneLoader(1);
+    }
+
+    public void Settings()
+    {
+        SettingsMenu.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Back()
+    {
+        SettingsMenu.SetActive(false);
+    }
+
+    public void SceneLoader(int SceneIndex)
+    {
+        SceneManager.LoadScene(SceneIndex);
+    }
+}
