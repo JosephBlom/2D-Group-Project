da3bbacd2d5af50d01ae149d2aa0de1350b9c6b1
JosephBlom <24.joseph.blom@ksd.org>
2023-10-25 12:57:38 -0700

    Updated the Boss Attack

--- 2D Group Project/Assets/Scripts/JosephTest.cs
+++ 2D Group Project/Assets/Scripts/JosephTest.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System.Collections;
 using System;
 using UnityEditor.Rendering;
+using JetBrains.Annotations;
 
 public class JosephTest : MonoBehaviour
 {
@@ -9,34 +10,61 @@ public class JosephTest : MonoBehaviour
     public GameObject player;
     public GameObject aoeZone;
     public GameObject aoeAttack;
+    public int bossHealth = 100;
 
-
+    bool alive = true;
     float timer = 0f;
-
 
     void Start()
     {
-        
-    }
 
+    }
+    
     void Update()
     {
-        timer += Time.deltaTime;
-        if (timer > 3f)
+        checkDie();
+        if (alive)
         {
-            bossAOE();
-            
+            timer += Time.deltaTime;
+            if (timer > 3f)
+            {
+                StartCoroutine(bossAOE());
+            }
         }
+        
     }
 
     IEnumerator bossAOE()
     {
         Vector3 playerPos = player.transform.position;
         GameObject aoeWarning = Instantiate(aoeZone, playerPos, Quaternion.identity);
+        timer = 0f;
         yield return new WaitForSeconds(2);
         Destroy(aoeWarning);
         GameObject aoeHitbox = Instantiate(aoeAttack, playerPos, Quaternion.identity);
-        timer = 0f;
+        yield return new WaitForSeconds(0.15f);
+        Destroy(aoeHitbox);
+    }
+
+    void checkDie()
+    {
+        if(bossHealth > 0)
+        {
+            alive = true;
+        }
+        else
+        {
+            alive = false;
+            Destroy(gameObject,2.5f);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Lantern"))
+        {
+            bossHealth -= 10;
+        }
     }
 }
 
