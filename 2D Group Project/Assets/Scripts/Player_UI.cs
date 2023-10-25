b7f09ef4b8404a1b518d791130663fdd935b9563
JosephBlom <24.joseph.blom@ksd.org>
2023-10-25 13:44:08 -0700

    Created a boss health bar

--- 2D Group Project/Assets/Scripts/Player_UI.cs
+++ 2D Group Project/Assets/Scripts/Player_UI.cs
@@ -6,15 +6,30 @@ using UnityEngine.UI;
 public class Player_UI : MonoBehaviour
 {
     public GameObject Player;
+    public GameObject Boss;
     PlayerShoot playerShoot;
     public Slider LanternSlider;
+    public Slider BossHealthSlider;
+    public JosephTest BossStats;
+
+    float BossHealth;
 
     void Start()
     {
         playerShoot = Player.GetComponent<PlayerShoot>();
+        BossHealthSlider.value = 2;
     }
     void Update()
     {
         LanternSlider.value = 2 - playerShoot.timer;
+        BossHurt();
+        
+    }
+
+    void BossHurt()
+    {
+        BossHealth = BossStats.bossHealth;
+        BossHealthSlider.value = (BossHealth / 100);
+        BossHealthSlider.gameObject.SetActive(BossHealthSlider.value > 0);
     }
 }
