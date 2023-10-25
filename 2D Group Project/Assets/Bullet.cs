dbfc2cc5efc161d849395be91911da7883f6e862
MarshmallowBoy <traysoncazier719@gmail.com>
2023-10-25 13:40:18 -0700

    Changed Tilemap

---
+++ 2D Group Project/Assets/Bullet.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject ParticleSystem1;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy") || collision.CompareTag("Player"))
+        {
+            return;
+        }
+        GameObject ParticleSystem2 = Instantiate(ParticleSystem1, transform.position, Quaternion.identity);
+        ParticleSystem2.GetComponent<ParticleSystem>().Play();
+        Destroy(gameObject);
+    }
+}
