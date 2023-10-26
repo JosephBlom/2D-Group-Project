bb003d451b85551e5fc727e3c7befc8f1a7c788d
JosephBlom <24.joseph.blom@ksd.org>
2023-10-26 13:56:29 -0700

    Fixed Boss Bar Bugs

--- 2D Group Project/Assets/Scripts/Player_UI.cs
+++ 2D Group Project/Assets/Scripts/Player_UI.cs
@@ -33,6 +33,10 @@ public class Player_UI : MonoBehaviour
         {
             BossHurt();
         }
+        else
+        {
+            BossHealthSlider.gameObject.SetActive(false);
+        }
         
         
     }
