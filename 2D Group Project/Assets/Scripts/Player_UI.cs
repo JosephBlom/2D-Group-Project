93627291ece7daa6908410929993bd3c7088e66c
JosephBlom <24.joseph.blom@ksd.org>
2023-10-26 11:53:32 -0700

    Updated the PlayerUI.cs
    Made the boss bar not appear/work if the boss is not in the level.

--- 2D Group Project/Assets/Scripts/Player_UI.cs
+++ 2D Group Project/Assets/Scripts/Player_UI.cs
@@ -11,18 +11,29 @@ public class Player_UI : MonoBehaviour
     public Slider LanternSlider;
     public Slider BossHealthSlider;
     public BossLogic BossStats;
+    bool bossHere;
+    GameObject[] temp;
 
     float BossHealth;
 
     void Start()
     {
+        temp = GameObject.FindGameObjectsWithTag("Boss");
+        if(temp.Length>0 )
+        {
+            bossHere = true;
+        }
         playerShoot = Player.GetComponent<PlayerShoot>();
         BossHealthSlider.value = 2;
     }
     void Update()
     {
         LanternSlider.value = 2 - playerShoot.timer;
-        BossHurt();
+        if (bossHere)
+        {
+            BossHurt();
+        }
+        
         
     }
 
