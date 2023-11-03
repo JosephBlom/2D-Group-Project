b3ff440a4eecd3142c1acc5933d230406ca6afca
MarshmallowBoy <traysoncazier719@gmail.com>
2023-11-03 12:22:24 -0700

    Water fix

--- 2D Group Project/Assets/Water.cs
+++ 2D Group Project/Assets/Water.cs
@@ -11,14 +11,17 @@ public class Water : MonoBehaviour
             collision.GetComponent<Rigidbody2D>().gravityScale = 2;
             collision.GetComponent<Rigidbody2D>().drag = 5;
             collision.GetComponent<PlayerMovement>().Swimming = true;
-            collision.GetComponent<PlayerMovement>().jumpSpeed = 5;
+            collision.GetComponent<PlayerMovement>().jumpSpeed = 10;
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        collision.GetComponent<Rigidbody2D>().gravityScale = 3;
-        collision.GetComponent<Rigidbody2D>().drag = 0;
-        collision.GetComponent<PlayerMovement>().Swimming = false;
-        collision.GetComponent<PlayerMovement>().jumpSpeed = 15;
+        if (collision.CompareTag("Player"))
+        {
+            collision.GetComponent<Rigidbody2D>().gravityScale = 3;
+            collision.GetComponent<Rigidbody2D>().drag = 0;
+            collision.GetComponent<PlayerMovement>().Swimming = false;
+            collision.GetComponent<PlayerMovement>().jumpSpeed = 15;
+        }
     }
 }
