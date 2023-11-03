e2c9209ec26e752f3c0e4f42f647b6d04408dcb9
JosephBlom <24.joseph.blom@ksd.org>
2023-11-03 14:00:35 -0700

    Created the toggle door tilemap

--- 2D Group Project/Assets/Scripts/JosephTest.cs
+++ 2D Group Project/Assets/Scripts/JosephTest.cs
@@ -4,39 +4,19 @@ using System;
 using UnityEditor.Rendering;
 using JetBrains.Annotations;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.Tilemaps;
 
 public class JosephTest : MonoBehaviour
 {
-    public Candle[] Candles;
-    public bool complete = true;
+    public CandleLogic candleLogic;
     void Start()
     {
-        Candles = FindObjectsOfType<Candle>();
+       candleLogic = GameObject.FindGameObjectsWithTag("MainCamera")[0].GetComponent<CandleLogic>();
     }
 
-    public void CheckPuzzle()
+    void Update()
     {
-        complete = true;
-        bool allLit = true; 
-        for (int i = 0; i < Candles.Length; i++)
-        {
-            if (!Candles[i].Value == Candles[i].isLit)
-            {
-                complete = false;
-            }
-            if (!Candles[i].isLit)
-            {
-                allLit = false;
-            }
-        }
-        if (!complete && allLit)
-        {
-            for(int i = 0;i < Candles.Length; i++)
-            {
-                Candles[i].isLit = false;
-                Candles[i].GetComponent<Light2D>().enabled = false;
-            }
-        }
+        gameObject.SetActive(candleLogic.complete);
     }
 }
 
