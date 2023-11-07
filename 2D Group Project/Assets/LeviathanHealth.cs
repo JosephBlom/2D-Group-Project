7d77abaf2f0eed129613611a505f2d434a036570
MarshmallowBoy <traysoncazier719@gmail.com>
2023-11-07 13:13:56 -0800

    LeviathanBossFinished

--- 2D Group Project/Assets/LeviathanHealth.cs
+++ 2D Group Project/Assets/LeviathanHealth.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LeviathanHealth : MonoBehaviour
 {
     public float health;
     public ParticleSystem Blood;
     public LeviathanScript LevScript;
+    public Slider Health;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Lantern"))
         {
             health--;
+            Health.value = health;
         }
         if (health <= 0)
         {
