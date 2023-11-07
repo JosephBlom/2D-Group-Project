dbc0e31d9c206c09401c309ef45bca75751d411a
MarshmallowBoy <traysoncazier719@gmail.com>
2023-11-07 14:00:00 -0800

    Pushing For The end of the day

---
+++ 2D Group Project/Assets/BurnableObject.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurnableObject : MonoBehaviour
+{
+    public GameObject fire;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Lantern"))
+        {
+            Instantiate(fire, transform.position, transform.rotation);
+            Destroy(collision.gameObject);
+        }
+    }
+}
