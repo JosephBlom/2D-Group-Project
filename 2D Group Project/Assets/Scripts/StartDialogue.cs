83fb36b793094d8941a9c03057c251bd9d9c6c1a
JosephBlom <24.joseph.blom@ksd.org>
2023-11-12 17:30:50 -0800

    Finished the dialogue and imported sound effects

---
+++ 2D Group Project/Assets/Scripts/StartDialogue.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartDialogue : MonoBehaviour
+{
+    public int waitSeconds = 5;
+    public GameObject dialogueTrigger;
+    void Start()
+    { 
+        StartCoroutine(startDialogue());
+    }
+    private IEnumerator startDialogue(){
+        Debug.Log("");
+        yield return new WaitForSeconds(waitSeconds);
+        dialogueTrigger.GetComponent<DialogueTrigger>().TriggerDialogue();
+    }
+}
