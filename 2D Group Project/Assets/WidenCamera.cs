445e5c69c8f91f0f1397c1722468da845e253d0c
JosephBlom <24.joseph.blom@ksd.org>
2023-11-16 13:10:24 -0800

    Edited Camera

--- 2D Group Project/Assets/WidenCamera.cs
+++ 2D Group Project/Assets/WidenCamera.cs
@@ -8,9 +8,9 @@ public class WidenCamera : MonoBehaviour
     public Camera mainCamera;
     void OnTriggerEnter2D(Collider2D collision)
     {
-        while(mainCamera.GetComponent<CinemachineVirtualCamera>().m_Lens.OrthographicSize < 10)
+        while (mainCamera.GetComponent<CinemachineVirtualCamera>().m_Lens.FieldOfView < 85)
         {
-            mainCamera.GetComponent<CinemachineVirtualCamera>().m_Lens.OrthographicSize += 0.1f;
+            mainCamera.GetComponent<CinemachineVirtualCamera>().m_Lens.OrthographicSize += 1f;
         }
     }
 }
