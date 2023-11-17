a36bc0b5c1ebccc0638d4ea91bc3bc5feb3a431c
MarshmallowBoy <traysoncazier719@gmail.com>
2023-11-17 12:09:58 -0800

    Update MainBoss.cs

--- 2D Group Project/Assets/Scripts/MainBoss.cs
+++ 2D Group Project/Assets/Scripts/MainBoss.cs
@@ -8,7 +8,7 @@ using UnityEngine.UI;
 
 public class MainBoss : MonoBehaviour
 {
-    PlayerHealth playerHealth
+    PlayerHealth playerHealth;
     public GameObject[] bossMoves;
     public Transform Player;
     public GameObject MeatBall;
